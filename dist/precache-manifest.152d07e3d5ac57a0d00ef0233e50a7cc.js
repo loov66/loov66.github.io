5481b93e83447be8d58e5dc308f619b5886b75a3
loov66 <touchylh@163.com>
2019-01-25 16:38:32 +0800

    set dir dist can upload

---
+++ dist/precache-manifest.152d07e3d5ac57a0d00ef0233e50a7cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1fb1d28191ebb4b93d7a",
+    "url": "/js/chunk-vendors.6dfe03e2.js"
+  },
+  {
+    "revision": "6a38757a5d2ea23a2e68",
+    "url": "/js/app.1239eeb6.js"
+  },
+  {
+    "revision": "367da63083ad72e14b4b",
+    "url": "/js/about.a807c8cf.js"
+  },
+  {
+    "revision": "2bdba51222c703d2022ee17fc620b181",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6a38757a5d2ea23a2e68",
+    "url": "/css/app.949d116c.css"
+  }
+];
